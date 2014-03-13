83aebb6b029801a768fabeacae06834a81010247
David Mackay <davidjx8p@gmail.com>
2014-03-13 19:40:57 +0000

    remove extraneous .h

--- BSDBuild_ref/BSDBuild/toxcore.pm
+++ BSDBuild_ref/BSDBuild/toxcore.pm
@@ -24,7 +24,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 # USE OF THIS SOFTWARE EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 my $testCode = << 'EOF';
-#include <tox/tox.h.h>
+#include <tox/tox.h>
 int main(int argc, char *argv[]) {
   return (0);
 }
