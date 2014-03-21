07e60c9012789cfdfa1fe411b277120158f2e685
Sean Qureshi <sean@tox.im>
2014-03-20 17:12:16 -0700

    Add commit number
    Signed-off-by: Sean Qureshi <sean@tox.im>

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@ TOXCORE=${TOXCORE_FILES:-"-ltoxcore"}
 TOXAV=${TOXAV_FILES:-"-ltoxav"}
 CRYPTO_LIB=${CRYPTO_LIB_FILES:-"-lsodium"}
 PREFIX=${PREFIX:-"/usr/local"}
-VERSION=0.3.1
+TOXIC_VERSION=0.3.1
+REV=`git rev-list HEAD --count`
+VERSION="${TOXIC_VERSION}_r${REV}"
+echo ${VERSION}
 
 
 (gcc -o toxic -D HAVE_WIDECHAR -DTOXICVER="${VERSION}" -D _SUPPORT_AUDIO ./src/*.c  -L"$PREFIX/lib/" -I"$PREFIX/include/" -lpthread -lopenal $TOXCORE $TOXAV $CRYPTO_LIB -lvpx -lncursesw -lopus && echo build with audio and widechar suceeded) || (gcc -o toxic -D _SUPPORT_AUDIO -DTOXICVER="${VERSION}" ./src/*.c  -L"$PREFIX/lib/" -I"$PREFIX/include/" -lpthread -lopenal $TOXCORE $TOXAV_FILES $CRYPTO_LIB -lvpx -lncurses -lopus && echo build with audio suceeded) || (gcc -o toxic -D HAVE_WIDECHAR -DTOXICVER="${VERSION}" ./src/{c,e,f,g,l,m,p,t,w}*.c  -L"$PREFIX/lib/" -I"$PREFIX/include/" -lpthread $TOXCORE $CRYPTO_LIB -lncursesw && echo build with widechar suceeded) || (gcc -o toxic -DTOXICVER="${VERSION}" ./src/{c,e,f,g,l,m,p,t,w}*.c  -L"$PREFIX/lib/" -I"$PREFIX/include/" -lpthread $TOXCORE $CRYPTO_LIB -lncurses && echo build suceeded) || echo build failed
